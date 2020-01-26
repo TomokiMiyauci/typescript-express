490877fbce00434aa86fd73eada953a957bd3af6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-26 23:34:51 +0900

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+    const timeText = new Date().toLocaleString("ja-JP")
+    document.querySelector("#currentTime").textContent = timeText
+}, 1000);
---
+++ vscode-works/README.md
@@ -0,0 +1,10 @@
+# VScode Works
+
+## Development list
+
+* README.md
+* Simple Clock
+* Todo application
+    * API Server  
+        * Tests
+* My Extension
